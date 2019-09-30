719a7a3a677168f3cf8ec49c5ad08ad5367a0129
Lucas Oliveira <lucasoliveira.si@outlook.com>
2019-09-30 17:47:19 -0300

    Heapsort

---
+++ src/Aula06/Heap_Sort/main.java
@@ -0,0 +1,31 @@
+package Aula06.Heap_Sort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Numeros Gerados:");
+        int [] aleatorio = new int[20];
+        Random r = new Random();
+
+        for(int i=0; i < aleatorio.length; i++){
+            aleatorio[i] = r.nextInt(100);
+        }
+
+        System.out.println(Arrays.toString(aleatorio));
+        Exercicio01 heapsort = new Exercicio01();
+        System.out.println("Numeros Ordenados: ");
+        heapsort.heapSort(aleatorio);
+        System.out.println(Arrays.toString(aleatorio));
+
+
+
+    }
+
+
+
+}
